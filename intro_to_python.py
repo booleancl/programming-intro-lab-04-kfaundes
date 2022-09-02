299279247bc8813ebefa2ff29c0e67702d832a94
unknown <kfaundesmadrid@gmail.com.com>
2022-09-02 10:09:53 -0500

    guardar cambios

---
+++ intro_to_python.py
@@ -0,0 +1,18 @@
+# Esto es un comentario
+
+# Esta es la unica funcion para imprimir un mensaje en la terminal
+print("hola mundo")
+# La indentacion es importante. Es la parte de la sintaxis
+# En este caso la linea  10 debe estar identada para ser parte de if
+
+
+if 5 > 2:
+print ("cinco es mayor q dos")
+# Cuando lleva comillas y no se ejecuta la expresion y cuando lleva comillas es texto
+Operaciones matematicas 
+print(40+2)
+print("40+2")
+
+print (7*6)
+print (42/6)
+
---
+++ variables.py
@@ -0,0 +1,22 @@
+# Una variable es un nombre que apunta a un valor
+# Los valores son de cierto tipo
+
+message = "Estos es un mensaje"
+
+age = 76
+
+#Los nombre de las variables no deben contener espacios
+# Los pueden separa con "_" o  underscore
+dolar_to_peso = 890.2
+
+print(message)
+print(age)
+print(dolar_to_peso)
+
+#Los nombres de las variables NO pueden  partir con mumeros
+
+# 76Trumpets = "Gran carnaval" => Esto es un nombre inválido
+
+# Tampoco puede utilizar las palabras reservadas del lenguaje
+
+# if = ¿si? => Esto tampoco es una nombre válido
